4ee23a50e0e0c4422d232a98fdd8fae5c9803008
Alexey Melezhik <melezhik@gmail.com>
2016-01-21 11:28:26 +0300

    up

--- mysql_user_select/story.pm
+++ mysql_user_select/story.pm
@@ -2,6 +2,8 @@ use DBI;
 
 my $hostname    = host();
 my $database    = 'mysql';
+my $port        = config()->{'mysql'}{'port'};
+
 my $user        = config()->{'mysql'}{'user'};
 my $password    = config()->{'mysql'}{'password'};
 
@@ -9,11 +11,27 @@ my $dsn = "DBI:mysql:database=$database;host=$hostname;port=$port";
  
 $dbh = DBI->connect($dsn, $user, $password);
 
-# now retrieve data from the table.
-my $sth = $dbh->prepare("SELECT Host,User FROM mysql.user WHERE User='root' AND Host='localhost'");
+warn $port;
+warn $user;
+warn $password;
+warn $hostname;
+warn $database;
+
+my $sth = $dbh->prepare("SELECT Host,User FROM mysql.user WHERE User='root' AND Host='127.0.0.1'");
 $sth->execute();
+
+set_stdout(qq{'SELECT Host,User FROM mysql.user WHERE User='root' AND Host='127.0.0.1'});
 while (my $ref = $sth->fetchrow_hashref()) {
   set_stdout("Host : $ref->{'Host'} User: $ref->{'User'}");
 }
 $sth->finish();
- 
+
+my $sth = $dbh->prepare("SELECT Host,User FROM mysql.user WHERE User='root' AND Host='localhost'");
+$sth->execute();
+
+set_stdout(qq{'SELECT Host,User FROM mysql.user WHERE User='root' AND Host='localhost'});
+while (my $ref = $sth->fetchrow_hashref()) {
+  set_stdout("Host : $ref->{'Host'} User: $ref->{'User'}");
+}
+$sth->finish();
+
